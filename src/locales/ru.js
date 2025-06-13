493005f8d0f407443c5410bd78440928731aa0e8
Ogurchik00 <nik.skripka.000@gmail.com>
2025-06-13 21:24:24 +0900

    fix7.9

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,14 +1,14 @@
-// src/locales/ru.js
 export default {
-    ru: {
-      translation: {
-        success: 'RSS успешно загружен',
-        errors: {
-          invalidUrl: 'Ссылка должна быть валидным URL',
-          duplicate: 'RSS уже существует',
-          invalidRss: 'Ресурс не содержит валидный RSS',
-          network: 'Ошибка сети',
-        },
+    translation: {
+      success: 'RSS успешно загружен',
+      errors: {
+        invalidUrl: 'Ссылка должна быть валидным URL',
+        duplicate: 'RSS уже существует',
+        invalidRss: 'Ресурс не содержит валидный RSS',
+        network: 'Ошибка сети',
       },
+      feeds: 'Фиды',
+      posts: 'Посты',
+      preview: 'Просмотр',
     },
   };
